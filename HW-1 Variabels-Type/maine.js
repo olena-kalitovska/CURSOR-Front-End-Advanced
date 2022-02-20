85986eb5f3d0f91065aee6e8959a8ade26c6a589
Olena Kalitovska <olena.kalitovska@gmail.com>
2022-02-20 15:22:25 +0200

    add: Boolean value of the sum of the value of goods

--- HW-1 Variabels-Type/maine.js
+++ HW-1 Variabels-Type/maine.js
@@ -1,4 +1,4 @@
-'use strick'
+'use strict'
 // Виведення максимального числа
 const arr = [15.678, 123.965, 90.2345];
 Math.max.apply(null, arr);
@@ -25,16 +25,15 @@ const theAmountOfFoodFixed = tangerines + huarma + garnet;
 theAmountOfFood.toFixed(3);
 console.log('Сума вартості товарів(округлення до сотень):',theAmountOfFoodFixed.toFixed(3));
 //Вивести булеве значення: Чи є сума всіх товарів(округлена в меншу сторону) паргим чи не парним числом?
-
-
-
+const theAmountOfFoodAll = Math.floor(theAmountOfFoodFull);
+console.log('Булеве значення суми вартості товарів(округлення в меншу сторну:',theAmountOfFoodAll % 2 === 0);
 //Вивести суму решти, при оплаті всіх товарі( без округлень), якщо клієнт платить 500 грн.
 const CLIENTS_PAY = 500;
 const clientsDelivery = CLIENTS_PAY - theAmountOfFoodFull;
 console.log('Решта клієнта, від 500 грн, при оплаті за всі товари:',clientsDelivery);
 //Вивести середнє значення цін, окркглене до другого знаку після коми.
 let sum = 0;
-for ( i = 0; i < arr.length; i++){
+for (let i = 0; i < arr.length; i++){
     sum += arr[i];
 };
 let result = sum / arr.length;
